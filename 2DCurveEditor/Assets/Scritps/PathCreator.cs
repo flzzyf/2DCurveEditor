e79e4adfe4f38aa75b682db77ebc75440a44a8dd
Flzzyf <2981019122@qq.com>
2019-12-17 17:54:02 +0800

    Init

---
+++ 2DCurveEditor/Assets/Scritps/PathCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour
+{
+    [HideInInspector]
+    public Path path;
+
+    public bool moveCorrespondingControlPoint = true;
+
+    public void CreatePath()
+    {
+        path = new Path(Vector2.zero);
+    }
+}
